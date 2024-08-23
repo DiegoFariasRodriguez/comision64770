13228e6d0a761094df8810508f5df6caba178c34
Diego Farias <diego_farias_92@live.com.ar>
2024-08-23 11:06:27 -0300

    preentrega2

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,49 @@
-console.log("Entro");
+function calcularDosis() {
+
+    const pesoInput = document.getElementById('peso').value;
+    const edad = document.getElementById('edad').value;
+    const contraste = document.getElementById('contraste').value;
+
+    const pesoKg = parseFloat(pesoInput);
 
 
-function calcularDosis(pesoKg) {
-    
     if (pesoKg <= 0 || isNaN(pesoKg)) {
-        return "El peso debe ser un número positivo.";
+        document.getElementById('resultado').innerText = "El peso debe ser un número positivo.";
+        return;
     }
 
-    // Definir la dosis por kilogramo (en mililitros)
-    const dosisPorKg = 1;
+    
+    let dosisPorKg = 0;
 
-    // Calcular la dosis total
+    if (edad === 'recién_nacido') {
+        if (contraste === 'ultravist_300') {
+            dosisPorKg = 4.0;
+        } else if (contraste === 'ultravist_370') {
+            dosisPorKg = 3.2;
+        }
+    } else if (edad === 'lactante') {
+        if (contraste === 'ultravist_300') {
+            dosisPorKg = 3.0;
+        } else if (contraste === 'ultravist_370') {
+            dosisPorKg = 2.7;
+        }
+    } else if (edad === 'niño') {
+        if (contraste === 'ultravist_300') {
+            dosisPorKg = 1.5;
+        } else if (contraste === 'ultravist_370') {
+            dosisPorKg = 1.4;
+        }
+    } else if (edad === 'adulto') {
+        if (contraste === 'ultravist_300') {
+            dosisPorKg = 1.0;
+        } else if (contraste === 'ultravist_370') {
+            dosisPorKg = 0.8;
+        }
+    }
+
+    
     const dosisTotal = pesoKg * dosisPorKg;
 
-    // Devolver el resultado
-    return `La dosis de contraste recomendada es ${dosisTotal} mililitros.`;
+    
+    document.getElementById('resultado').innerText = `La dosis de contraste recomendada es ${dosisTotal.toFixed(2)} mililitros.`;
 }
-
